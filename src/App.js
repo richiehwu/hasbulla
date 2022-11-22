6f0b211799025cff33c7843fd0001e80dc051032
Richie <richiehwu@gmail.com>
2022-11-22 22:12:17 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [num,setNum] = useState(1)
+  function add(){
+    console.log("hello")
+    setNum(num+1)
+    
+
+  }
+  function minus(){
+    setNum(num-1)
+  }
+
+  return (
+    <div className="App">
+      <h1>
+        Punt Hasbulla
+      </h1>
+      <p>
+        Times punted: {num}
+      </p>
+      <button onClick={add}>
+        punt
+      </button>
+      <button onClick={minus}>
+        reduce
+      </button>
+      <div>
+        <a href='https://www.google.com'>
+          hello
+        </a>
+        <img src='https://www.mansworldindia.com/wp-content/uploads/2022/06/Hasbulla.jpg' alt="Hasbulla with glasses"/>
+
+
+      </div>
+       
+    </div>
+  );
+}
+
+export default App;
